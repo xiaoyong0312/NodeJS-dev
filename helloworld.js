2bb711ff26692b8740b7221c209fd5010926a61f
xuyong <xuyong@qianmi.com>
2016-08-15 09:41:58 +0800

    NodeJS-dev,监听端口的服务,代码整理;

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,11 @@ var http = require("http");
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html;charset:utf-8"});
   response.write("Hello World! This is helloworld.js!");
-  response.write('<head><meta charset="utf-8"/></head>');
+  response.write('<head>' +
+    '<title>NodeJS-dev</title>' +
+    '<meta charset="utf-8"/>' +
+    '<link href="/images/NodeJS-dev-index-1.1.ico" rel="icon" type="image/x-icon"/>' +
+    '</head>');
   response.write('<h1></h1>');
   response.write('<b></b>');
   response.end();
